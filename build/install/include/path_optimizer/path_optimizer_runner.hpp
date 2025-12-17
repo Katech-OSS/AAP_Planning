38baaf5e2d82d194bcc1132181b6f3d3609e4336
jjh <jjh@katech.re.kr>
2025-12-17 23:43:20 +0900

    TEST SW

--- build/install/include/path_optimizer/path_optimizer_runner.hpp
+++
@@ -1,8 +0,0 @@
-// Compatibility wrapper so includes using "path_optimizer/path_optimizer_runner.hpp"
-// still work after the header was moved to include/path_optimizer_runner.hpp
-#ifndef PATH_OPTIMIZER_PATH_OPTIMIZER_RUNNER_HPP
-#define PATH_OPTIMIZER_PATH_OPTIMIZER_RUNNER_HPP
-
-#include "../path_optimizer_runner.hpp"
-
-#endif // PATH_OPTIMIZER_PATH_OPTIMIZER_RUNNER_HPP
